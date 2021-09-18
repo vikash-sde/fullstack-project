006a14149d1c6c8c5f207f950159d4d30d7368b5
Vikash Gupta <vikashg2512@gmail.com>
2021-09-18 22:16:10 +0530

    mern stack project

---
+++ note.md
@@ -0,0 +1,4 @@
+vikash
+vikash1234
+
+mongodb+srv://vikash:<password>@cluster0.qfnsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
---
+++ server/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+
+const PORT = 8000;
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log("no connection"));
+
+//Middleware
+
+const middleware = (req, res, next) => {
+  console.log("hello from middleware");
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.send("hello from the server");
+});
+
+app.get("/about", middleware, (req, res) => {
+  console.log("hello from About");
+
+  res.send("hello from the about");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("hello from the contact");
+});
+
+app.get("/signin", (req, res) => {
+  res.send("hello from the signin");
+});
+
+app.get("/signup", (req, res) => {
+  res.send("hello from the signup");
+});
+
+app.listen(PORT, () => {
+  console.log("listening at " + PORT);
+});
