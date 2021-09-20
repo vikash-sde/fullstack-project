06ef5108935060532ebf61e5516a4c8f55d66fbc
Vikash Gupta <vikashg2512@gmail.com>
2021-09-20 13:11:00 +0530

    Update

--- server/app.js
+++ server/app.js
@@ -27,27 +27,27 @@ const middleware = (req, res, next) => {
 
 //Routing
 
-app.get("/", (req, res) => {
-  res.send("hello from the server");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello from the server");
+// });
 
-app.get("/about", middleware, (req, res) => {
-  console.log("hello from About");
+// app.get("/about", middleware, (req, res) => {
+//   console.log("hello from About");
 
-  res.send("hello from the about");
-});
+//   res.send("hello from the about");
+// });
 
-app.get("/contact", (req, res) => {
-  res.send("hello from the contact");
-});
+// app.get("/contact", (req, res) => {
+//   res.send("hello from the contact");
+// });
 
-app.get("/signin", (req, res) => {
-  res.send("hello from the signin");
-});
+// app.get("/signin", (req, res) => {
+//   res.send("hello from the signin");
+// });
 
-app.get("/signup", (req, res) => {
-  res.send("hello from the signup");
-});
+// app.get("/signup", (req, res) => {
+//   res.send("hello from the signup");
+// });
 
 app.listen(PORT, () => {
   console.log("listening at " + PORT);
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,75 @@
 const express = require("express");
 
 const router = express.Router();
+require("../db/connection");
+const User = require("../model/userschema");
 
 router.get("/", (req, res) => {
   res.send("hello from the server");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  //query
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "all the field are required" });
+  }
+
+  try {
+    const userExist = User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "user already exists" });
+    }
+    // If key and value are samet than
+    const user = new User({ name, email, phone, work, password, cpassword });
+    const userRegister = await user.save();
+    // await user.save();
+    // res.status(201).json({ success: "user created successfully" });
+
+    if (userRegister) {
+      res.status(201).json({ success: "user created successfully" });
+    } else {
+      res.status(500).json({ error: "registration failed" });
+    }
+  } catch (err) {
+    console.log(error);
+  }
+
   console.log(req.body);
-  res.json({ message: req.body });
+  //   res.json({ message: req.body });
 });
+
+// router.post("/register", (req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+
+//   //query
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(422).json({ error: "all the field are required" });
+//   }
+//   // Promises
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({ error: "user already exists" });
+//       }
+//       // If key and value are samet than
+//       const user = new User({ name, email, phone, work, password, cpassword });
+//       user
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ success: "user created successfully" });
+//         })
+//         .catch((error) => {
+//           res.status(500).json({ error: "registration failed" });
+//         });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+
+//   console.log(req.body);
+//   //   res.json({ message: req.body });
+// });
 
 module.exports = router;
